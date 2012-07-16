cb07b4c75cc82e7fa8688bf68a83798871ca94ee
Adam Phelps <amp@cs.stanford.edu>
2012-07-15 20:41:20 -0700

    * Remoted DEBUG_VERBOSE to individual sketches

--- Debug/Debug.h
+++ Debug/Debug.h
@@ -6,7 +6,6 @@
  */
 
 #ifdef DEBUG
-  #define DEBUG_VERBOSE 2
   #define DEBUG_PRINT(v, x) if (v <= DEBUG_VERBOSE) Serial.print(x)
   #define DEBUG_PRINT_HEX(v, x) if (v <= DEBUG_VERBOSE) Serial.print(x, HEX)
   #define DEBUG_WRITE(v, x) if (v <= DEBUG_VERBOSE)  Serial.print(x, HEX);
