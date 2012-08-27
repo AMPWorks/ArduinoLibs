9d682959370c3fae300cbe6515c45b66ea2e6afb
Adam Phelps <amp@cs.stanford.edu>
2012-08-26 17:05:29 -0700

    * Added #def'd debug levels

--- Debug/Debug.h
+++ Debug/Debug.h
@@ -5,6 +5,12 @@
  * before including this header.
  */
 
+// Print levels
+#define DEBUG_ERROR 0
+#define DEBUG_LOW   1
+#define DEBUG_MID   2
+#define DEBUG_HIGH  3
+
 #ifdef DEBUG
   #define DEBUG_PRINT(v, x) if (v <= DEBUG_VERBOSE) Serial.print(x)
   #define DEBUG_PRINT_HEX(v, x) if (v <= DEBUG_VERBOSE) Serial.print(x, HEX)
