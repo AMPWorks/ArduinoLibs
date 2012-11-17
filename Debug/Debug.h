4c6230d3fbc517dc58c7a4d0b02deb19faabff0f
Adam Phelps <amp@cs.stanford.edu>
2012-11-17 12:03:17 -0800

    * Further improve DEBUG logging

--- Debug/Debug.h
+++ Debug/Debug.h
@@ -16,9 +16,13 @@
   #define DEBUG_PRINT_HEX(v, x) if (v <= DEBUG_VERBOSE) Serial.print(x, HEX)
   #define DEBUG_WRITE(v, x) if (v <= DEBUG_VERBOSE)  Serial.print(x, HEX)
   #define DEBUG_COMMAND(x) x;
+  #define DEBUG_VALUE(v, x, y) if (v <= DEBUG_VERBOSE) {        \
+      Serial.print(x); Serial.print(y);                         \
+    }
 #else
   #define DEBUG_PRINT(v, x)
   #define DEBUG_PRINT_HEX(v, x)
   #define DEBUG_WRITE(v, x)
   #define DEBUG_COMMAND(x)
+  #define DEBUG_VALUE(v, x, y)
 #endif
