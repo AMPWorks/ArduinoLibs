fa39d7b7aa3e49a20408b8f3f76ddda0c8599efe
Adam Phelps <amp@cs.stanford.edu>
2013-06-21 02:09:55 -0700

    Initial version of RS485Utils

---
+++ RS485Utils.cpp
@@ -0,0 +1,75 @@
+
+#include <RS485_non_blocking.h>
+#include <SoftwareSerial.h>
+
+#include "RS485Utils.h"
+
+SoftwareSerial serialglobal(2, 3);
+SoftwareSerial *serial;
+
+RS485Socket::RS485Socket(byte _recvPin, byte _xmitPin, byte _enablePin) 
+{
+  recvPin = _recvPin;
+  xmitPin = _xmitPin;
+  enablePin = _enablePin;
+
+  serial = new SoftwareSerial(recvPin, xmitPin);
+
+  channel = new RS485(serialRead, serialAvailable, serialWrite, 0 /* XXX */ );
+}
+
+
+void RS485Socket::setup() 
+{
+  
+  serial->begin(28800);
+  channel->begin();
+  
+  pinMode(enablePin, OUTPUT);
+
+  digitalWrite(enablePin, LOW);
+}
+
+size_t RS485Socket::serialWrite(const byte what) 
+{
+  Serial.print("write:");
+  Serial.println(what);
+  return serial->write(what);
+}
+
+int RS485Socket::serialRead() 
+{
+  return serial->read();
+}
+
+int RS485Socket::serialAvailable() 
+{
+  return serial->available();
+}
+
+void RS485Socket::sendMsgTo(byte address, const byte * data, const byte length) 
+{
+  // XXX: Add address data
+  Serial.print("XMIT:");
+  Serial.print(*data);
+  Serial.print("-");
+  Serial.println(length);
+
+  digitalWrite(enablePin, HIGH);
+  channel->sendMsg(data, length);
+  digitalWrite(enablePin, LOW);
+}
+
+const byte *RS485Socket::getMsg(byte address) 
+{
+  if (channel->update()) {
+//    int datalen = channel->getLength();
+    const byte *data = channel->getData();
+
+    // XXX: Check if message destination matches address
+
+    return data;
+  } else {
+    return NULL;
+  }
+}
---
+++ RS485Utils.h
@@ -0,0 +1,28 @@
+#ifndef RS485UTILS_H
+#define RS485UTILS_H
+
+#include "Arduino.h"
+
+#include <RS485_non_blocking.h>
+#include <SoftwareSerial.h>
+
+class RS485Socket 
+{
+  public:
+  RS485Socket(byte _recvPin, byte _xmitPin, byte _enablePin);
+  void setup();
+
+  void sendMsgTo(byte address, const byte * data, const byte length);
+  const byte *getMsg(byte address);
+
+  private:
+  byte recvPin, xmitPin, enablePin;
+
+  RS485 *channel;
+
+  static size_t serialWrite(const byte what);
+  static int serialRead();
+  static int serialAvailable();
+};
+
+#endif
