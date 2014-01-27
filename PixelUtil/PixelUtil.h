22f056829d14b6ae3a6b1f5ffcfe86f981f0023e
Adam Phelps <amp@opendns.com>
2014-01-27 01:40:13 -0800

    Fixed new init function

--- PixelUtil/PixelUtil.h
+++ PixelUtil/PixelUtil.h
@@ -44,7 +44,7 @@ class PixelUtil
   PixelUtil(uint16_t numPixels, uint8_t dataPin, uint8_t clockPin,
             uint8_t order=WS2801_RGB);
   void init(uint16_t numPixels, uint8_t dataPin, uint8_t clockPin,
-	    uint8_t order);
+	    uint8_t order=WS2801_RGB);
   
   uint32_t pixelColor(byte r, byte g, byte b);
 
