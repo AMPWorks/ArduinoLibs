d244524f394dec8a0267e939e00ebe3b6aeea9eb
Adam Phelps <amp@opendns.com>
2014-02-03 10:28:55 -0800

    Added DEBUG_ERROR_ONLY debug level

--- Debug/Debug.h
+++ Debug/Debug.h
@@ -2,6 +2,9 @@
  * In order to enable this code in a sketch, place the following:
  *   #define DEBUG_LEVEL <level>
  * before including this header.
+ *
+ * Enabling debugging will increase PROGMEM usage as all strings are stored
+ * in progmem.
  */
 
 // Print levels
@@ -10,12 +13,22 @@
 #define DEBUG_MID   2
 #define DEBUG_HIGH  3
 #define DEBUG_TRACE 4
+#define DEBUG_ERROR_ONLY -1 // Will only compile error commands
 
 #ifdef DEBUG /* Compatibility with earlier Debug.h syntax */
-  #define DEBUG_LEVEL DEBUG_VERBOSE 
+  #define DEBUG_LEVEL DEBUG_HIGH
 #endif
 
 #ifdef DEBUG_LEVEL
+#if DEBUG_LEVEL == DEBUG_ERROR_ONLY
+  #define DEBUG_ERR(x)   Serial.println(F(x));
+  #define DEBUG_ERR_STATE(x) debug_err_state(x);
+#else
+  #define DEBUG
+#endif
+#endif
+
+#ifdef DEBUG
   extern char close_line;
 
   #define DEBUG_PRINT(v, x) if (v <= DEBUG_LEVEL) {                     \
@@ -53,6 +66,8 @@
 
   #define DEBUG_ERR(x) DEBUG_PRINTLN(DEBUG_ERROR, x);
 
+  #define DEBUG_ERR_STATE(x) debug_err_state(x);
+
   #define DEBUG_COMMAND(v, x) if (v <= DEBUG_LEVEL) {     \
     x; \
   }
@@ -65,15 +80,19 @@
   #define DEBUG_PRINTLN(v, x)
   #define DEBUG_PRINT_HEX(v, x)
   #define DEBUG_WRITE(v, x)
-  #define DEBUG_COMMAND(x)
   #define DEBUG_VALUE(v, x, y)
   #define DEBUG_HEX(v, x)
   #define DEBUG_HEXVAL(v, x, y)
   #define DEBUG_VALUELN(v, x, y)
   #define DEBUG_PRINT_END()
-  #define DEBUG_ERR(x)
   #define DEBUG_COMMAND(v, x)
   #define DEBUG_MEMORY(v)
+  #ifndef DEBUG_ERR
+    #define DEBUG_ERR(x)
+  #endif
+  #ifndef DEBUG_ERR_STATE
+    #define DEBUG_ERR_STATE(x)
+  #endif
 #endif
 
 /* Error codes */
@@ -81,8 +100,6 @@
 #define DEBUG_ERR_UNINIT  0x11
 #define DEBUG_ERR_INVALID 0x12
 #define DEBUG_ERR_REINIT 0x12
-
-#define DEBUG_ERR_STATE(x) debug_err_state(x);
 
 /* Flash an error code */
 void debug_err_state(int code);
