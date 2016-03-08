0b4c440151441e192d08d27ddc1d3d2deb52d7bb
Adam Phelps <amp@opendns.com>
2016-03-07 23:35:51 -0800

    Improved the SerialCLI example

--- SerialCLI/examples/ExampleCLI/ExampleCLI.ino
+++ SerialCLI/examples/ExampleCLI/ExampleCLI.ino
@@ -9,7 +9,10 @@
 
 void handler(char **tokens, byte numtokens);
 
-SerialCLI serialcli(32, handler);
+SerialCLI serialcli(
+        32,     // Max command length, this amount is allocated as a buffer
+        handler // Function for handling tokenized commands
+);
 
 void setup() {
   Serial.begin(9600);
@@ -19,6 +22,14 @@ void loop() {
   serialcli.checkSerial();
 }
 
+void print_usage() {
+  Serial.print(F(
+" \n"
+"Usage:\n"
+ "  h - print this help\n"
+ "  s <value> - Send a value\n"
+));
+}
 void handler(char **tokens, byte numtokens) {
   Serial.print(numtokens);
   Serial.print(" tokens: ");
@@ -27,4 +38,22 @@ void handler(char **tokens, byte numtokens) {
     Serial.print(tokens[token]);
   }
   Serial.println();
+
+  switch (tokens[0][0]) {
+
+    case 'h': {
+      print_usage();
+      break;
+    }
+
+    case 's': {
+      if (numtokens < 2) return;
+      uint16_t value = atoi(tokens[1]);
+      Serial.print("* Sensor request to: ");
+      Serial.println(value);
+      break;
+    }
+
+  }
 }
+
